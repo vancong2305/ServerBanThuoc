872dff2f2c852ed895fdbc5b8763b522d11a2f9f
VanuLL <truongvancong230502@gmail.com>
2024-01-17 22:17:30 +0700

    init project

---
+++ src/main/java/com/ServerBanThuoc/ServerBanThuocApplication.java
@@ -0,0 +1,13 @@
+package com.ServerBanThuoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBanThuocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBanThuocApplication.class, args);
+	}
+
+}
